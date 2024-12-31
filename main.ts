13b189d2a4b99c0a93663645cf3b8626c63629d1
Fast4word <152832715+Fast4word@users.noreply.github.com>
2024-12-31 13:50:53 +1300

    Update main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
-let _player = game.createSprite(2, 2)
+let _player1 = game.createSprite(2, 2)
+let _player2 = game.createSprite(3, 2)
 let _ball = game.createSprite(2, 2)
+let time = 120
 music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
 _ball.set(LedSpriteProperty.Y, -6)
 basic.forever(function () {
     basic.pause(500)
     _ball.change(LedSpriteProperty.Y, 1)
+})
+basic.forever(function () {
+    time += 0 - 1
+    if (true) {
+    	
+    }
 })
