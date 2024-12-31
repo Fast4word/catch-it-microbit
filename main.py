5d98cb55efe60d05b0ea247d57ad84c21ab2d4f6
Fast4word <152832715+Fast4word@users.noreply.github.com>
2024-12-31 13:51:22 +1300

    Update pxt.json, main.blocks, main.py

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
 _player1 = game.create_sprite(2, 2)
 _player2 = game.create_sprite(3, 2)
 _ball = game.create_sprite(2, 2)
-time = 120
-time -= 1
-music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
-    music.PlaybackMode.UNTIL_DONE)
-_ball.set(LedSpriteProperty.Y, -6)
-
-def on_forever():
-    basic.pause(500)
-    _ball.change(LedSpriteProperty.Y, 1)
-basic.forever(on_forever)
